d9e6e950efe187965dada0b8b88fed3c66fc265e
khaled <khalednaguib55555@gmail.com>
2024-08-24 16:41:47 +0300

    new branch

--- Airbnb.Domain/Entities/Booking.cs
+++ Airbnb.Domain/Entities/Booking.cs
@@ -10,6 +10,7 @@ namespace Airbnb.Domain.Entities
 {
 	public class Booking : BaseEntity<int>
 	{
+		//
 		public decimal TotalPrice { get; set; }
 		public DateTimeOffset StartDate { get; set; }
 		public DateTimeOffset EndDate { get; set; }
