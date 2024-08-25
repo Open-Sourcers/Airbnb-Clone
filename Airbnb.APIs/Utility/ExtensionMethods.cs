28e7319e12ce7c7215891c908d53ac8809e48b42
SharaawyMohamed <sharawem585@gmail.com>
2024-08-25 17:11:44 +0300

    Complete Generec Creatsion + Applying migrations When Program un

---
+++ Airbnb.APIs/Utility/ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using Airbnb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Writers;
+
+namespace Airbnb.APIs.Utility
+{
+    public static class ExtensionMethods
+    {
+        // To Create Data base if not exist
+        public static async Task ApplyMigrations(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                // create object of Db context
+                var service = scope.ServiceProvider;
+                var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var _context = service.GetRequiredService<AirbnbDbContext>();
+                    var unAppliedMigrations = await _context.Database.GetPendingMigrationsAsync();
+                    if(unAppliedMigrations.Any())
+                    {
+                        await _context.Database.MigrateAsync();// Apply All pending migrations on database database
+                    }
+                  // can we call data seeding method here after we Apply all  pending migrations
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ Airbnb.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Airbnb.Domain;
+using Airbnb.Domain.Interfaces.Repositories;
+using Airbnb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airbnb.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly AirbnbDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AirbnbDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+           => await _dbSet.AddAsync(entity);
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+          => await _dbSet.AddRangeAsync(entities);
+
+        public void Remove(TEntity entity)
+           => _dbSet.Remove(entity);
+
+
+        public async Task<IEnumerable<TEntity>>? GetAllAsync()
+          => await _dbSet.ToListAsync();
+
+
+        public async Task<TEntity>? GetByIdAsync(TKey id)
+          => (await _dbSet.FindAsync(id))!;
+
+
+        public void Update(TEntity entity)
+          => _dbSet.Update(entity);
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+         => _dbSet.UpdateRange(entities);
+    }
+}
