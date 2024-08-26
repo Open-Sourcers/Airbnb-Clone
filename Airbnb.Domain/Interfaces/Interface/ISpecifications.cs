b3c1400c18960a8fb94ff0ad76cfc7bb020ad414
Ali-nasser1 <popa0251@gmail.com>
2024-08-26 18:22:51 +0300

    Apply Specification Pattern

---
+++ Airbnb.Domain/Interfaces/Interface/ISpecifications.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Airbnb.Domain;
+
+namespace Airbnb.Domain.Interfaces.Interface
+{
+    public interface ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public Expression<Func<TEntity, bool>> Criteria { get; set; }
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; }
+        public Expression<Func<TEntity, object>> OrderBy { get; set; }
+        public Expression<Func<TEntity, object>> OrderByDescending { get; set; }
+
+        public int Take { get; set; }
+        public int Skip {  get; set; }
+        public bool IsPaginationEnabled { get; set; }
+    }
+}
