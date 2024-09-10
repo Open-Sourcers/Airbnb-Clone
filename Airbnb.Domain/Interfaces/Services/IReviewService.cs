92ce7d5c104bef3cb43359f974586ad5c62e7da6
khaled <khalednaguib55555@gmail.com>
2024-09-10 19:42:20 +0300

    Reviews services and controllers

---
+++ Airbnb.Domain/Interfaces/Services/IReviewService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airbnb.Domain.Entities;
+
+namespace Airbnb.Domain.Interfaces.Services
+{
+    public interface IReviewService
+    {
+        Task<Review> GetReviewAsync(int id);
+        Task<IEnumerable<Review>> GetAllReviewsAsync();
+        Task<IEnumerable<Review>> GetReviewsByPropertyIdAsync(int productId);
+        Task AddReviewAsync(Review review);
+        Task DeleteReviewAsync(int id);
+
+    }
+}
