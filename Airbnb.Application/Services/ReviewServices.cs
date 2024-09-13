0761b1d35f3f99ac3d7081b417c4ebf2baceff62
khaled <khalednaguib55555@gmail.com>
2024-09-13 08:18:02 +0300

    implemnting response class

--- Airbnb.Application/Services/ReviewServices.cs
+++ Airbnb.Application/Services/ReviewServices.cs
@@ -11,27 +11,26 @@ namespace Airbnb.Application.Services
 {
     public class ReviewServices : IReviewService
     {
+        private readonly IUnitOfWork _unitOfWork;
 
-        private readonly IGenericRepository<Review, int> _genericRepository;
-        private readonly IGenericRepository<Property, int> _genericPropertyRepository;
-        public ReviewServices(IGenericRepository<Review, int> genericRepository, IGenericRepository<Property, int> genericPropertyRepository)
+        public ReviewServices(IUnitOfWork unitOfWork)
         {
-            _genericRepository = genericRepository;
-            _genericPropertyRepository = genericPropertyRepository;
-
+            _unitOfWork = unitOfWork;
         }
+
         public async Task AddReviewAsync(Review review)
         {
-            await _genericRepository.AddAsync(review);
+            await _unitOfWork.Repository<Review, int>().AddAsync(review);
+            await _unitOfWork.CompleteAsync();
         }
 
         public async Task DeleteReviewAsync(int id)
         {
-
-            var review = await _genericRepository.GetByIdAsync(id);
-           if (review != null)
+            var review = await _unitOfWork.Repository<Review, int>().GetByIdAsync(id);
+            if (review != null)
             {
-                _genericRepository.Remove(review);
+                _unitOfWork.Repository<Review, int>().Remove(review);
+                await _unitOfWork.CompleteAsync();
             }
             else
             {
@@ -41,18 +40,24 @@ namespace Airbnb.Application.Services
 
         public async Task<IEnumerable<Review>> GetAllReviewsAsync()
         {
-            return await _genericRepository.GetAllAsync(); }
-    
-        public Task<Review> GetReviewAsync(int id)
-        {
-            var review= _genericRepository.GetByIdAsync(id);
-            return review;
+            return await _unitOfWork.Repository<Review, int>().GetAllAsync();
         }
 
-        public async Task<IEnumerable<Review>> GetReviewsByPropertyIdAsync(int PropertyId)
+        public async Task<Review> GetReviewAsync(int id)
         {
-            var property = await _genericPropertyRepository.GetByIdAsync(PropertyId);
+            return await _unitOfWork.Repository<Review, int>().GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Review>> GetReviewsByPropertyIdAsync(int propertyId)
+        {
+            var property = await _unitOfWork.Repository<Property, int>().GetByIdAsync(propertyId);
+            if (property == null)
+            {
+                throw new KeyNotFoundException($"Property with ID {propertyId} not found.");
+            }
+
             return property.Reviews;
-        }    
+        }
     }
+
 }
